beab263716096c6405e210a9f264d5be24e20783
Lucius VanSlyke <lu.vanslyke@gmail.com>
2020-10-26 05:17:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,8 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |element, index|
-    index + 1
-    puts "#{index}. #{element}"
+    starting_index = index + 1
+    puts "#{starting_index}. #{element}"
   end
 
 end
